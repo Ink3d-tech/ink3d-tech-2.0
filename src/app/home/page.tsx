1c63f56b6b4d5db7fb0b8bfb8cf20a2d1d4fcae1
biagio360 <facundobiagioni@gmail.com>
2025-02-21 17:07:32 -0300

    componente UpButton, perfeccionamiento home,inicio de estructura renderizado de productos traidos del back

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,25 +1,28 @@
-import Buttons from "@/app/home/Buttons.component";
+// import Buttons from "@/app/home/Buttons.component";
 import Carousel from "@/app/home/Carousel.component";
 import ProductList from "@/app/home/ProductList.component";
 import BackButton from "@/shared/components/BackButton.component";
+import ScrollToTop from "@/shared/components/UpButton.component";
+import ButtonsMini from "./ButtonsMini.component";
 
 export default function Home() {
   return (
     
     <div className="flex flex-col  bg-gray-300">
-      <div className='bg-black h-12 flex items-center px-3'>
-        <BackButton />
-        <h1 className='text-some-gray ml-2 text-base'>Start</h1>
+      <div className="mb-6">
+        <BackButton tab="Inicio"/>
       </div>
       <Carousel imageIds={[1, 2, 3]} />
-      <Buttons />
-      <ProductList category="buzos"/>
+      <ButtonsMini />
+      {/* <Buttons /> */}
+      <ProductList category="remeras" />
 
       <Carousel imageIds={[4, 5, 6]} />
-      <ProductList category="remeras" />
+      <ProductList category="buzos"/>
 
       <Carousel imageIds={[7, 8, 9]} />
       <ProductList category="pantalones"/>
+      <ScrollToTop />
     </div>
   );
 }
