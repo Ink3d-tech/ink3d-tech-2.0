e405c710552f4b70a68d2d34e44da3f0781b51ed
ITAlonsoRabanal <nachoalonso273@gmail.com>
2025-02-27 14:22:13 -0300

    mp cart wip

--- src/modules/checkout/pages/cart/services/cart.services.ts
+++ src/modules/checkout/pages/cart/services/cart.services.ts
@@ -1,21 +1,17 @@
 import axios from "axios";
-import { IFilteredCart } from "../interfaces/Cart.interface";
+import { IProduct } from "../interfaces/Cart.interface";
 import { API_BACK } from "@/shared/config/api/getEnv";
 
 
-export const confirmOrderService = async (userBuyer: string, filteredCart: IFilteredCart[], token: string | null) => {
+export const confirmOrderService = async (userBuyer: string, confirmedCart: IProduct[], token: string | null) => {
     try {
         const body = {
             userId: userBuyer,
-            products: filteredCart.map(({ id, units }) => ({
+            products: confirmedCart.map(({ id, units }) => ({
                 id,
                 quantity: units
             }))
         };
-        
-        console.log(body, token);
-        
-
         const { data } = await axios.post(`${API_BACK}/orders`, body, 
             { 
                 headers: { 
@@ -24,10 +20,42 @@ export const confirmOrderService = async (userBuyer: string, filteredCart: IFilt
                 }
             }
         );
-
         return data;
     } catch (error) {
         console.error("Error confirming order:", error);
+        throw error;
+    }
+};
+
+export const paymentCreate = async (orderId: string, currency: string, confirmedCart: { id: string; name: string; price: number }[], token: string) => {
+    try {
+        const body = {
+            orderId,
+            currency,
+            products: confirmedCart.map(({ id, name, price }) => ({
+                id,
+                name,
+                price
+            }))
+        };
+
+        console.log("Enviando pago:", JSON.stringify(body, null, 2));
+        console.log("Llamando a:", `${API_BACK}/payments`);
+
+        const { data } = await axios.post(
+            `${API_BACK}/payments`, 
+            body,
+            { 
+                headers: { 
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`
+                }
+            }
+        );
+
+        return data;
+    } catch (error: any) {
+        console.error("Error creando el pago:", error.response?.data || error);
         throw error;
     }
 };
