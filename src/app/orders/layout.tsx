1747ce5c848a85bf8e395861ff4b8133d8dcf0d4
Camiloc7 <j.camilocorredor@gmail.com>
2025-03-07 15:40:52 -0500

    quitar provedor de layout orders

--- src/app/orders/layout.tsx
+++ src/app/orders/layout.tsx
@@ -10,9 +10,7 @@ export const metadata: Metadata = {
 export default function DashboardLayout({children}: {children: React.ReactNode}) {
     return (
         <ProtectedRoute title={"Hello! To view your dashboard, please log in to your account"}>
-            <OrderProvider>  
                 {children}
-            </OrderProvider>
         </ProtectedRoute>
     );
 }
